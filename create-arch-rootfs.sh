0c71da7293ad91dd6b133d810d8033f751719a65
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-10-14 06:27:27 -0400

    cleanup
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- create-arch-rootfs.sh
+++ create-arch-rootfs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # https://blog.herecura.eu/blog/2020-05-21-toying-around-with-firecracker/
-# set -eu -o pipefail
+set -eu -o pipefail
 
 NAME=$1
 
@@ -23,19 +23,7 @@ sudo pacstrap /mnt/arch-root base \
   sudo
 
 sudo mkdir -p /mnt/arch-root/home/mccurdyc/
-# mkdir -p /mnt/arch-root/home/mccurdyc/.ssh
-# mkdir -p /mnt/arch-root/home/mccurdyc/.tools
-# mkdir -p /mnt/arch-root/home/mccurdyc/.cache/yay
-#
 sudo cp -r $(pwd)/etc /mnt/arch-root
-#
-# cp /home/mccurdyc/.ssh/id_ed25519.pub /mnt/arch-root/home/mccurdyc/.ssh
-# cp /home/mccurdyc/.ssh/id_ed25519 /mnt/arch-root/home/mccurdyc/.ssh
-# cp /home/mccurdyc/.ssh/config /mnt/arch-root/home/mccurdyc/.ssh
-#
-# cp -r /home/mccurdyc/dotfiles /mnt/arch-root/home/mccurdyc/dotfiles
-# cp -r /opt/yay /mnt/arch-root/home/mccurdyc/.tools/yay
-sudo hostnamectl hostname fc-arch
 
 sudo rm /mnt/arch-root/etc/systemd/system/getty.target.wants/*
 sudo rm /mnt/arch-root/etc/systemd/system/multi-user.target.wants/*
@@ -43,14 +31,8 @@ sudo rm /mnt/arch-root/etc/systemd/system/multi-user.target.wants/*
 sudo ln -s /dev/null /mnt/arch-root/etc/systemd/system/systemd-random-seed.service
 sudo ln -s /dev/null /mnt/arch-root/etc/systemd/system/cryptsetup.target
 
-# arch-chroot /mnt/arch-root locale-gen en_US
-# arch-chroot /mnt/arch-root chmod 0400 /etc/shadow /etc/gshadow
-# arch-chroot /mnt/arch-root chown -R mccurdyc /home/mccurdyc
 arch-chroot /mnt/arch-root systemctl enable --now systemd-networkd.service
 arch-chroot /mnt/arch-root systemctl enable --now sshd.service
-# arch-chroot /mnt/arch-root bash -c 'cd /home/mccurdyc/.tools/yay; runuser -u mccurdyc -- makepkg -si --noconfirm' # can't run as root
-# arch-chroot /mnt/arch-root bash -c 'cd /home/mccurdyc/dotfiles; runuser -u mccurdyc -- make run'
-# sudo arch-chroot /mnt/arch-root passwd -d root
 
-# sudo umount /mnt/arch-root
-# sudo rmdir /mnt/arch-root
+sudo umount /mnt/arch-root
+sudo rmdir /mnt/arch-root
